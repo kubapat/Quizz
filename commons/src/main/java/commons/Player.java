676dbf5c0cf2d70eb22b93e85374053861ab1095
Rares Toader <R.A.Toader@student.tudelft.nl>
2022-03-05 14:36:14 +0100

    Adds a player class and its repository
    Makes a player class that has its username as an identifier and its repository. Also tests the class thoroughly.

---
+++ commons/src/main/java/commons/Player.java
@@ -0,0 +1,64 @@
+package commons;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Player {
+
+    @Id
+    private String username;
+
+    private long score;
+
+    private Player() {
+        //for object mapper
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getScore() {
+        return score;
+    }
+
+    public void setScore(long score) {
+        this.score = score;
+    }
+
+    public Player(String username) {
+        this.username = username;
+        this.score = 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Player)) return false;
+        Player player = (Player) o;
+        return getScore() == player.getScore() && getUsername().equals(player.getUsername());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getScore());
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "username='" + username + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    public void incrementScore(long amount) {
+        this.score += amount;
+    }
+}
