27097623c823d2c3f20de2144b38fd9038b60a69
Yannick Rijsenbrij <Y.O.Rijsenbrij@student.tudelft.nl>
2022-03-05 13:08:35 +0100

    Added a timer and points
    Made a timer that counts down from 30 to 0. Created a point system based on how much time is left, scaling equally between 1000 for 30 seconds left and 500 for 0 seconds left (so 15 seconds left will give you 750 points).

---
+++ commons/src/main/java/commons/Points.java
@@ -0,0 +1,15 @@
+package commons;
+
+public class Points {
+
+    int currPoints = 0;
+    Countdown countdown = new Countdown();
+
+    public int getPoints(boolean answered){
+        if(answered){
+            int getTime = countdown.getCurrTime();
+            currPoints = 500 + (500/30 * getTime);
+        }
+        return currPoints;
+    }
+}
