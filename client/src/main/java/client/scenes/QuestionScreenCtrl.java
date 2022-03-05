172a9bf76d6c25a6fb82b04fe86398c07215b465
Yannick Rijsenbrij <Y.O.Rijsenbrij@student.tudelft.nl>
2022-03-05 12:15:15 +0100

    See correct answer
    Highlights the background of the correct answer green. If the player answer is wrong, it will be highlighted in red

---
+++ client/src/main/java/client/scenes/QuestionScreenCtrl.java
@@ -0,0 +1,52 @@
+package client.scenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class QuestionScreenCtrl {
+
+    public String chosenAnswer;
+    public String correctAnswer;
+    public int points = 0;
+
+    @FXML
+    private Button firstChoice;
+
+    @FXML
+    private Button secondChoice;
+
+    @FXML
+    private Button thirdChoice;
+
+
+    public void chooseFirst(){
+        chosenAnswer = firstChoice.toString();
+        setBackground(firstChoice, secondChoice, thirdChoice);
+    }
+
+    public void chooseSecond(){
+        chosenAnswer = secondChoice.toString();
+        setBackground(secondChoice, firstChoice, thirdChoice);
+    }
+
+    public void chooseThird(){
+        chosenAnswer = thirdChoice.toString();
+        setBackground(thirdChoice, firstChoice, secondChoice);
+    }
+
+    // Changes the color of the background so the player can see if they answered the question correctly
+    public void setBackground(Button yourAnswer, Button nextAnswer, Button lastAnswer){
+        if(chosenAnswer.equals(correctAnswer)){
+            yourAnswer.setStyle("-fx-background-color: green;");
+        }
+        else{
+            yourAnswer.setStyle("-fx-background-color: red;");
+            if(correctAnswer.equals(nextAnswer.toString())){
+                nextAnswer.setStyle("-fx-background-color: green;");
+            }
+            else{
+                lastAnswer.setStyle("-fx-background-color: green;");
+            }
+        }
+    }
+}
