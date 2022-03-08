9a0319c41804c2c7f88b59ce759d7fc0bf914b4c
Rares Toader <R.A.Toader@student.tudelft.nl>
2022-03-08 21:51:39 +0100

    Optimise imports -> pipeline fix

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -15,23 +15,16 @@
  */
 package client.utils;
 
-import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.List;
-
 import commons.Activity;
 import commons.Player;
-import jakarta.ws.rs.core.Application;
-import org.glassfish.jersey.client.ClientConfig;
-
-import commons.Quote;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.GenericType;
+import org.glassfish.jersey.client.ClientConfig;
+
+import java.util.List;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class ServerUtils {
 
