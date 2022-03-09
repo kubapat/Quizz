268133cf4e1676da11725f33fd6a56790200f8b2
Rares Toader <R.A.Toader@student.tudelft.nl>
2022-03-09 13:31:41 +0100

    Link splash to queue screen
    Links splash to the queue screen via the multiplayer and cancel button respectively.

--- client/src/main/java/client/scenes/QueueCtrl.java
+++ client/src/main/java/client/scenes/QueueCtrl.java
@@ -1,9 +1,14 @@
 package client.scenes;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 import javax.inject.Inject;
 
 public class QueueCtrl {
     private final MainCtrl mainCtrl;
+    @FXML
+    private Button goBack;
     @Inject
     public QueueCtrl(MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
