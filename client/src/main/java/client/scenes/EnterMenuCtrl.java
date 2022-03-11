081cca13b7f208c0f2472b5884f3ef9fd1699106
Rares Toader <r.a.toader@student.tudelft.nl>
2022-03-11 22:26:05 +0100

    Update EnterMenuCtrl.java

--- client/src/main/java/client/scenes/EnterMenuCtrl.java
+++ client/src/main/java/client/scenes/EnterMenuCtrl.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
+import client.ServerUtils;
 import javax.inject.Inject;
 
 public class EnterMenuCtrl {
