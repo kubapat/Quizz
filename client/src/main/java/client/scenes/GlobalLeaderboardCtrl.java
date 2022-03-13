8a6043e2645d3561d336cd0694e1aaa02c8c7419
Arthur Jacques <A.J.P.Jacques@student.tudelft.nl>
2022-03-13 16:37:31 +0100

    pipeline cmon

--- client/src/main/java/client/scenes/GlobalLeaderboardCtrl.java
+++ client/src/main/java/client/scenes/GlobalLeaderboardCtrl.java
@@ -2,20 +2,14 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import commons.Player;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.util.Callback;
-
 import javax.inject.Inject;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class GlobalLeaderboardCtrl {
 
