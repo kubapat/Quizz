6c2a449ce7bf8c38bd58e6989f6a4a2d656d4187
Yannick Rijsenbrij <Y.O.Rijsenbrij@student.tudelft.nl>
2022-03-15 13:32:26 +0100

    now QuizzQuestionServerParsed

--- server/src/main/java/server/SessionController.java
+++ server/src/main/java/server/SessionController.java
@@ -34,7 +34,7 @@ public class SessionController {
         Session x = SessionContainer.getSession(sessionId);
 
         if(x.hasEnded()) {
-            return new QuizzQuestion("0",null,null,null);
+            return new QuizzQuestionServerParsed(new QuizzQuestion("0",null,null,null),null,-1);
         }
 
         if(!x.isStarted()) { //If game is not started
