083d3a6fd2f995f0670b3b0bf2328452a727e889
michielbakker <m.s.bakker-1@student.tudelft.nl>
2022-03-16 17:13:40 +0100

    made the set-up for the Admin Panel

--- client/src/main/java/client/scenes/AdminPanelCtrl.java
+++ client/src/main/java/client/scenes/AdminPanelCtrl.java
@@ -2,13 +2,10 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import commons.Player;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 
 import javax.inject.Inject;
 
@@ -16,14 +13,6 @@ public class AdminPanelCtrl {
 
     private final ServerUtils serverUtils;
     private final MainCtrl mainCtrl;
-    @FXML
-    private Button goBack;
-    @FXML
-    private TableView<Player> tableView;
-    @FXML
-    private TableColumn<Player,String> nameColumn;
-    @FXML
-    private TableColumn<Player,Long> scoreColumn;
 
     @Inject
     public AdminPanelCtrl(MainCtrl mainCtrl, ServerUtils serverUtils) {
@@ -35,11 +24,4 @@ public class AdminPanelCtrl {
         mainCtrl.showSplash();
     }
 
-    public void initialize() {
-        nameColumn.setCellValueFactory(new PropertyValueFactory("username"));
-        scoreColumn.setCellValueFactory(new PropertyValueFactory("score"));
-        ObservableList<Player> players = FXCollections.observableArrayList();
-        players.addAll(serverUtils.getAllPlayers());
-        tableView.setItems(players);
-    }
 }
