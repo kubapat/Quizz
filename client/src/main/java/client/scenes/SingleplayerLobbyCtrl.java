eebee74641b52c012730af9784c87ad37bb5932c
Francisco Ruas Vaz <f.ruasvaz@student.tudelft.nl>
2022-03-22 15:28:56 +0100

    Give a count down before starting game

--- client/src/main/java/client/scenes/SingleplayerLobbyCtrl.java
+++ client/src/main/java/client/scenes/SingleplayerLobbyCtrl.java
@@ -1,9 +1,13 @@
 package client.scenes;
 
 import client.utils.ServerUtils;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import javax.inject.Inject;
 
@@ -11,6 +15,7 @@ public class SingleplayerLobbyCtrl {
     private MainCtrl mainCtrl;
     private final ServerUtils serverUtils;
     private GlobalLeaderboardCtrl globalLeaderboardCtrl;
+    private int transitionTimeLeft;
 
     @Inject
     public SingleplayerLobbyCtrl(MainCtrl mainCtrl,ServerUtils serverUtils,GlobalLeaderboardCtrl globalLeaderboardCtrl) {
@@ -27,6 +32,8 @@ public class SingleplayerLobbyCtrl {
     public Button startGameButton;
     @FXML
     private Button leaderboardButton;
+    @FXML
+    private Text label1;
 
     public void leaderboardButton() {
         globalLeaderboardCtrl.init();
@@ -34,7 +41,25 @@ public class SingleplayerLobbyCtrl {
     }
 
     public void toSingleplayer() {
-        mainCtrl.showSingleplayer();
+        transitionTimeLeft = 5;
+        label1.setText("Starting in " + transitionTimeLeft + "!");
+        Timeline timerForStart = new Timeline(
+                new KeyFrame(Duration.seconds(1),
+                        event -> {
+//                            System.out.println("transitionTimeLeft = " + transitionTimeLeft); //DEBUG LINE
+                            if (transitionTimeLeft == 0) {
+                                label1.setText("Play by yourself and place on the global leaderboard!");
+                                mainCtrl.showSingleplayer();
+                            }
+                            else {
+                                transitionTimeLeft -= 1;
+                                label1.setText("Starting in " + transitionTimeLeft + "!");
+                            }
+                        }
+                )
+        );
+        timerForStart.setCycleCount(6);
+        timerForStart.play();
     }
 
     public void goBackToSplash() {
